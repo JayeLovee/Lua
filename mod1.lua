67b37c4c46bf6966898f38cd8fee5dee1460efd6
JayeLovee <81439257+JayeLovee@users.noreply.github.com>
2022-05-21 05:35:05 -0400

    Create mod1.lua

---
+++ mod1.lua
@@ -0,0 +1,13 @@
+print("Module 1 - Print")
+print("Hello World")
+-- single line comment
+
+--[[
+    this is a multi
+    line comment
+]]
+
+--concat with ..
+print("Hello " .. "Jack")
+
+--open this with "mod 1.lua"
